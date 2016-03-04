7f57e3e3c02a6dfa7d7092d7c9951445be58614e
Eric Schraeder <eshawk23@gmail.com>
2016-03-03 22:17:02 -0700

    solved 8puzzle problem

--- 8Puzzle/src/Board.java
+++ 8Puzzle/src/Board.java
@@ -1,120 +1,131 @@
-import java.util.Arrays;
+/*
+ * Author: Eric Schraeder 3/2016
+ * Algorithms I Princeton University via coursera.org
+ * Programming assignment III - 8 Puzzle
+ * 
+ * The 8-puzzle problem is a puzzle invented and popularized by Noyes Palmer Chapman in the 1870s. 
+ * It is played on a 3-by-3 grid with 8 square blocks labeled 1 through 8 and a blank square. Goal is to rearrange the blocks so that they are in order, 
+ * using as few moves as possible. You are permitted to slide blocks horizontally or vertically into the blank square. 
+ * 
+ * This project solves the 8-puzzle problem (and its natural generalizations) using the A* search algorithm.
+ * 
+ * Board.java - represents a 8-puzzle problem board
+ * Solver.java - implements the A* search algorithm 
+ * 
+ */
+
 import java.util.Stack;
 
 public class Board {
 	
-	private int N;
-	private int zeroI;
-	private int zeroJ;
+	//size of board
+	private short N;
+	
+	//location of the open space "0" in the puzzle
+	private short zeroI;
+	private short zeroJ;
 
-	//this 
-	private final int[][] board;
+	//2D array representation of the board
+	private final short[][] board;
 	
-	//used for generating the neighbor boards
-	private int[][] neighborBlocks;
-	private int[][] twinBoard;
-	private final int[] boardAsArray;
-	private Stack<Board> neighborBoards;
-	
-	// construct a board from an N-by-N array of blocks
-    // (where blocks[i][j] = block in row i, column j)
-	// constructs a 1D array and a twin board 
+	// construct a board from an N-by-N array of integers
     public Board(int[][] blocks) {
     	
-    	N = blocks.length;
-    	neighborBoards = new Stack<Board>();
-    	boardAsArray = new int[N * N];
-    	board = new int[N][N];
-    	twinBoard = new int[N][N];
-    	neighborBlocks = new int[N][N];
-   
+    	N = (short) blocks.length;
+    	board = new short[N][N];
+  
     	for (int i = 0; i < N; i++) {
     		for (int j = 0; j < N; j++) {
-    			boardAsArray[N * i + j] = blocks[i][j];
-    			board[i][j] = blocks[i][j];
-    			twinBoard[i][j] = blocks[i][j];
-    			neighborBlocks[i][j] = blocks[i][j];
+    			board[i][j] = (short) blocks[i][j];
     			if (blocks[i][j] == 0) {
-    				zeroI = i;
-    				zeroJ = j;
+    				zeroI = (short) i;
+    				zeroJ = (short) j;
     			}
     		}
     	}
-    	swapPairOfBlocks();	
+ 
     }
-    //create new boards by swapping 0 with surrounding values    
-    //check current position of 0 to generate valid boards
-    //minimum of 2 boards will be generated, max of 4
-    //  00 01 02 03
-    //  10 11 12 13
-    //  20 21 22 23
-    //  30 31 32 33
-    private void createNeighbors(int zeroI, int zeroJ) {
-    	//case 00
+    
+    /*
+     *   createNeighbors parameters:  i and j locations of 0 for this board,
+     *   int 2D array for creating new Boards, stack to store all possible neighbors of this Board 
+     *   
+     *   checks the location of the zero/open space for valid moves
+     *   calls the appropriate exchangeZeroWith<POSITION>() method(s) to create all valid neighbor Boards
+     *   neighbor Boards pushed to stack 
+     *   
+     */
+    private void createNeighbors(int zeroI, int zeroJ, int[][] neighbors, Stack<Board> neighborBoards) {
+    	//cases values = i x j
+    	//case 0 x 0
     	if (zeroI == 0 && zeroJ == 0) {
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
     		return;
     	}
-    	//case 03
+    	//case 0 x N - 1
     	else if (zeroI == 0 && zeroJ == N-1) {
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
     		return;
     	}
-    	//case 30
+    	//case N-1 x 0
     	else if (zeroI == N-1 && zeroJ == 0) {
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
     		return;
     	}
-    	//case 33
+    	//case N-1 x N-1
     	else if (zeroI == N-1 && zeroJ == N-1) {
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
     		return;	
     	}
     	//case L side of board
     	else if (zeroJ == 0) {
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
     		return;
     	}
     	//case R side of board
     	else if (zeroJ == N-1) {
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
     		return;
     	}
     	//case Top of board
     	else if (zeroI == 0) {
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
     		return;
     	}
     	//case Bottom of board
     	else if (zeroI == N-1) {
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
     		return;
     	}
     	//0 not along any edges of the board
     	//4 exchanges and 4 possible neighbor boards
     	else {
-    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ));
-    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ));
+    		neighborBoards.push(exchangeZeroWithLeft(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithRight(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithAbove(zeroI, zeroJ, neighbors));
+    		neighborBoards.push(exchangeZeroWithBelow(zeroI, zeroJ, neighbors));
     		return;
     	}
     }
     
-    //takes [i][j] = location of 0
-    private Board exchangeZeroWithAbove(int i, int j) {
+    /*
+     *  The following 'exchange' methods create the neighbor Boards of this Board
+     *  a 'neighbor' is a valid move as defined for 8-puzzle problem
+     *  methods swap the values with zero/open space and return a new neighbor Board
+     */
+    private Board exchangeZeroWithAbove(int i, int j, int[][] neighborBlocks) {
     	int temp = neighborBlocks[i-1][j];
 		neighborBlocks[i][j] = temp;
 		neighborBlocks[i-1][j] = 0;
@@ -124,7 +135,7 @@ public class Board {
 		return b;
     }
     
-    private Board exchangeZeroWithBelow(int i, int j) {
+    private Board exchangeZeroWithBelow(int i, int j, int[][] neighborBlocks) {
     	int temp = neighborBlocks[i+1][j];
 		neighborBlocks[i][j] = temp;
 		neighborBlocks[i+1][j] = 0;
@@ -133,7 +144,7 @@ public class Board {
 		neighborBlocks[i+1][j] = temp;
 		return b;
     }
-    private Board exchangeZeroWithLeft(int i, int j) {
+    private Board exchangeZeroWithLeft(int i, int j, int[][] neighborBlocks) {
     	int temp = neighborBlocks[i][j-1];
 		neighborBlocks[i][j] = temp;
 		neighborBlocks[i][j-1] = 0;
@@ -142,7 +153,7 @@ public class Board {
 		neighborBlocks[i][j-1] = temp;
 		return b;
     }
-    private Board exchangeZeroWithRight(int i, int j) {
+    private Board exchangeZeroWithRight(int i, int j, int[][] neighborBlocks) {
     	int temp = neighborBlocks[i][j+1];
 		neighborBlocks[i][j] = temp;
 		neighborBlocks[i][j+1] = 0;
@@ -154,7 +165,7 @@ public class Board {
     
     //swap pair of blocks to create the twin board
     //will be used to test if the original board is unsolvable
-    private void swapPairOfBlocks() {
+    private void swapPairOfBlocks(int[][] twinBoard) {
     	if( (twinBoard[0][0] != 0) && (twinBoard[0][1] != 0) ) {
     		int temp = twinBoard[0][0];
     		twinBoard[0][0] = twinBoard[0][1];
@@ -166,20 +177,28 @@ public class Board {
     		twinBoard[1][1] = temp;
     	}
     }
-    // board dimension N
+    
     public int dimension() {
     	return N;
     }
     
-    // number of blocks out of place
+    /*
+     * Hamming priority function. The number of blocks in the wrong position, plus the number of moves made so far to get to the search node. 
+     * Intuitively, a search node with a small number of blocks in the wrong position is close to the goal, 
+     * and we prefer a search node that have been reached using a small number of moves.
+     */
     public int hamming() {
     	int count = 0;
-    	for (int i = 0; i < N * N; i++) {
-    		if ( (boardAsArray[i] != (i+1)) && (boardAsArray[i] != 0) ) count++;
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < N; j++) {
+    			if(board[i][j] != 0 && board[i][j] != (N*i + j + 1)) count++;
+    		}
     	}
     	return count;
     }
-    // sum of Manhattan distances between blocks and goal
+    
+    // returns the Manhattan distance for this Board
+    // Manhattan distance = distance between two points is the sum of the absolute differences of their Cartesian coordinates. 
     public int manhattan()  {
     	int goal_i;
     	int goal_j;
@@ -196,25 +215,51 @@ public class Board {
     	}
     	return manhattan_sum;
     }
-    // is this board the goal board?
+    
+
     public boolean isGoal()   {
     	return this.hamming() == 0;
     }
+    
     // a board that is obtained by exchanging any pair of blocks
     public Board twin()  {
-    	return new Board(twinBoard);	
+    	int[][] twin = new int[N][N];
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < N; j++) {
+    			twin[i][j] = board[i][j];
+    		}
+    	}
+    	swapPairOfBlocks(twin);
+    	return new Board(twin);
     }
-    // does this board equal y?
+    
+ 
     public boolean equals(Object y)  {
     	if (y == null) return false;
     	if (this.getClass() != y.getClass()) return false;
     	Board that = (Board) y;
-    	return Arrays.deepEquals(this.board, that.board);
+    	for(int i = 0; i < N; i++) {
+    		for (int j = 0; j < N; j++) {
+    			if (this.board[i][j] != that.board[i][j])
+    				return false;
+    		}
+    	}
+    	return true;
     }
  
+    // returns an Iterable containing all the neighbors of this board
+    // neighbors new Boards that represent all valid moves of the 8-puzzle problem from current Board permutation
     public Iterable<Board> neighbors() {
-    	createNeighbors(zeroI, zeroJ);
-    	return neighborBoards;
+    	Stack<Board> neighborsStack = new Stack<Board>();
+    	
+    	int[][] neighbors = new int[N][N];
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < N; j++) {
+    			neighbors[i][j] = board[i][j];
+    		}
+    	}
+    	createNeighbors(zeroI, zeroJ, neighbors, neighborsStack);
+    	return neighborsStack;
     }
 
     // string representation of this board (in the output format specified below)
@@ -230,15 +275,19 @@ public class Board {
 		return s.toString();
 	}
 
-    public static void main(String[] args) {
-    	int[][] bricks = {{8, 1, 3},{4, 0, 2},{7, 6, 5}};
+   // Test client for Board.java
+   public static void main(String[] args) {
+    	int[][] bricks = {{1, 0, 3},{4, 5, 2},{7, 6, 8}};
+    	int[][] bricks2 = {{1, 3, 0},{4, 5, 2},{7, 6, 8}};
     	Board board = new Board(bricks);
+    	Board board2 = new Board(bricks2);
+    	System.out.println(board.equals(board2));
     	System.out.println(board.hamming());
     	System.out.println(board.manhattan());
     	System.out.println(board.isGoal());
     	System.out.println(board.toString());
     	System.out.println("----------");
-    	//System.out.println(board.twin().toString());
+    	System.out.println(board.twin().toString());
     	for (Board b : board.neighbors()) {
     		System.out.println(b.toString());
     	}
--- 8Puzzle/src/Solver.java
+++ 8Puzzle/src/Solver.java
@@ -1,3 +1,27 @@
+/*
+ * Author: Eric Schraeder 3/2016
+ * Algorithms I Princeton University via coursera.org
+ * Programming assignment III - 8 Puzzle
+ * 
+ * The 8-puzzle problem is a puzzle invented and popularized by Noyes Palmer Chapman in the 1870s. 
+ * It is played on a 3-by-3 grid with 8 square blocks labeled 1 through 8 and a blank square. Goal is to rearrange the blocks so that they are in order, 
+ * using as few moves as possible. You are permitted to slide blocks horizontally or vertically into the blank square. 
+ * 
+ * This project solves the 8-puzzle problem (and its natural generalizations) using the A* search algorithm.
+ * 
+ * Board.java - represents a 8-puzzle problem board
+ * Solver.java - uses general method as defined by A* search algorithm 
+ * 
+ * A* is an informed search algorithm, or a best-first search, 
+ * meaning that it solves problems by searching among all possible paths to the solution (goal) 
+ * for the one that incurs the smallest cost (least distance traveled, shortest time, etc.), 
+ * and among these paths it first considers the ones that appear to lead most quickly to the solution.
+ * 
+ * Solver.java uses a minimum priority queue of nodes that implement a Manhattan priority function
+ * 
+ */
+
+
 import edu.princeton.cs.algs4.*;
 
 public class Solver {
@@ -5,8 +29,14 @@ public class Solver {
 	private MinPQ<SolverNode> minPriorityQue;
 	private SolverNode solution;
 	private Board original;
-	private int movesToSolve;
 	
+	/* SolverNode defines a search node of the game to be a board, a priority
+	 * the number of moves made to reach the board, and the previous search node.
+	 * 
+	 * SolverNode priority is defined as a boards Manhattan distance + moves
+	 * Manhattan priority function. The sum of the Manhattan distances (sum of the vertical and horizontal distance) from the blocks to their goal positions, 
+	 * plus the number of moves made so far to get to the search node.
+	 */
 	private static class SolverNode implements Comparable<SolverNode> {
 		private Board board;
 		private int priority;
@@ -20,40 +50,63 @@ public class Solver {
 			previous = prev;
 		}
 		
-		@Override
 		public int compareTo(SolverNode o) {
-			if (this.priority < o.priority) return -1;
-			else if (this.priority > o.priority) return 1;
-			else return 0;
+			if (this.priority < o.priority) 
+				return -1;
+			if (this.priority > o.priority) 
+				return 1;
+			if (this.board.hamming() > o.board.hamming()) 
+				return -1;
+			if (this.board.equals(o.board))
+				return 0;
+			else return 1;
 		}
+	
 	}
-	// find a solution to the initial board (using the A* algorithm)
+	
+	
     public Solver(Board initial)  {
+    	if (initial == null) throw new NullPointerException();
+    	
     	original = initial;
+    	
     	minPriorityQue = new MinPQ<SolverNode>();
+    	
     	SolverNode init = new SolverNode(initial, 0, null);
     	SolverNode twinInit = new SolverNode(initial.twin(), 0, null);
-    	minPriorityQue.insert(init);
+
     	minPriorityQue.insert(twinInit);
-    	movesToSolve = 0;
+    	minPriorityQue.insert(init);
+    	
     	while (true) {
     		solution = minPriorityQue.delMin();
     		if (solution.board.isGoal()) break;
-    		movesToSolve++;
+    		
     		for (Board b : solution.board.neighbors()) {
-    			if (movesToSolve == 1) {
-    				minPriorityQue.insert(new SolverNode(b, movesToSolve, solution));
-    			}
-    			else if (movesToSolve > 1) {
-    				if (! b.equals(solution.previous.board)) {
-    					minPriorityQue.insert(new SolverNode(b, movesToSolve, solution));
-    				}
+    			if (! thisBoardAlreadyUsed(b, solution)) {
+    				minPriorityQue.insert(new SolverNode(b, solution.moves + 1, solution));
     			}
     		}
     	}
     	
+    	
     }
+    
+    //check if the Board b is already been added to the game tree
+    private boolean thisBoardAlreadyUsed(Board b, SolverNode base) {
+    	SolverNode curr = base;
+    	while (curr.previous != null) {
+    		if (curr.board.equals(b) || curr.board.equals(original)) {
+    			return true;
+    		}
+    		curr = curr.previous;
+    		
+    	}
+    	return false;
+    }
+    
     // is the initial board solvable?
+    // detect unsolvable puzzles 
     public boolean isSolvable() {
     	SolverNode cur = solution;
     	while (cur.previous != null) {
@@ -65,9 +118,10 @@ public class Solver {
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-    	if (isSolvable()) return movesToSolve;
+    	if (isSolvable()) return solution.moves;
     	else return -1;
     }
+    
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution()  {
     	if (! isSolvable() ) return null;
@@ -79,10 +133,12 @@ public class Solver {
     		stack.push(cur.board);
     		cur = cur.previous;
     	}
+ 
     	stack.push(cur.board);
     	return stack;
     }
     
+    //test client
     public static void main(String[] args) {
 
         // create initial board from file
